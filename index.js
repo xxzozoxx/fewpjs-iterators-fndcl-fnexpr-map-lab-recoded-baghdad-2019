7f01255cd086bcc4901452fbc5871143f3aa0108
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-09-29 17:50:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ let tutorials = [
 ];
 function titleCased (tutorials){
  tutorials = tutorials.map(function(v){
-  let newTutorials=[];
   let arr = v.toLowerCase().split(' ');
   for(let i=0; i<arr.length;i++){
     arr[i]=arr[i].charAt(0).toUpperCase()+arr[i].slice(1);
-     newTutorials = arr.join(' ');
+    
   }
-  return (tutorials = newTutorials);
+   v = arr.join(' ');
+     return v;
   });
   return tutorials;
 }
