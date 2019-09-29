ca14fbf8834d0319424dd8b66d6267fdbafbdefa
Zahraa  Jalaa <zahraa.jalaa99@gmail.com>
2019-09-29 17:54:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let tutorials = [
 ];
   let arr = [];
  tutorials = tutorials.map(function(v){
-   arr = v.toLowerCase().split(' ');
+   arr = v.split(' ');
   for(let i=0; i<arr.length;i++){
     arr[i]=arr[i].charAt(0).toUpperCase()+arr[i].slice(1);
     
